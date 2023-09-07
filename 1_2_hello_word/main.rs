87b95cf9da0f6f3c34a543f1b118099cfe0109ec
alibreo3754 <starpro123@naver.com>
2023-09-07 21:59:12 +0900

    README 추가

--- 1_2_hello_word/main.rs
+++ 1_2_hello_word/main.rs
@@ -2,6 +2,8 @@
 // () -> 파라미터 선언위치 {} -> 함수 바디
 // main() { <- 이게 추천하는 컨벤션
 // rustfmt라는 lint? 같은 도구도 있는듯
+// rust는 a head of time compiled 언어라서 빌드를 하면 실행 파일이 생긴다.
+
 fn main() {
   println!("Hello, world");
 }
---
+++ README.md
@@ -0,0 +1,3 @@
+# 러스트 언어 학습용 스터디 기록
+
+- reference: https://doc.rust-lang.org/stable/book/
