581eb05da822ea793e4ce7b5d3f6f544c05a3c86
pray.123 <pray.123@kakaoenterprise.com>
2023-09-12 14:31:41 +0900

    야구게임

--- DataType_3_2/target/debug/deps/DataType_3_2-9de8cd9cb53c508d.d
+++ DataType_3_2/target/debug/deps/DataType_3_2-9de8cd9cb53c508d.d
@@ -1,5 +1,5 @@
-/Users/pray/rust/StudyRust/DataType_3_2/target/debug/deps/DataType_3_2-9de8cd9cb53c508d.rmeta: src/main.rs
+/Users/pray.123/work/rust/StudyRust/DataType_3_2/target/debug/deps/DataType_3_2-9de8cd9cb53c508d.rmeta: src/main.rs
 
-/Users/pray/rust/StudyRust/DataType_3_2/target/debug/deps/DataType_3_2-9de8cd9cb53c508d.d: src/main.rs
+/Users/pray.123/work/rust/StudyRust/DataType_3_2/target/debug/deps/DataType_3_2-9de8cd9cb53c508d.d: src/main.rs
 
 src/main.rs:
